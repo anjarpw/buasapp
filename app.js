8bd5832375d5e585b9da4713c95565b5313a7dfd
anjarpw <anjar.p.wicaksono@gmail.com>
2015-05-12 21:49:08 +0800

    FRESH DEPLOYMENT

--- app.js
+++ app.js
@@ -8,40 +8,13 @@ var engines = require('consolidate');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var engines = require('consolidate');
-var grunt = require('grunt');
-var mongoose = require('mongoose');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 app.engine('html', require('ejs').renderFile);
 app.engine('jade', require('jade').__express);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1/sampledb');
-// var db=mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function (callback) {
-//   console.log('Connection Established');  
-// });
-// var Cat = mongoose.model('Cat', { name: String });
-// var kitty = new Cat({ name: 'Zildjian' });
-// kitty.save(function (err) {
-//   if (err) // ...
-//   console.log('meow');
-// });
-// var User = mongoose.model('User', { name: String });
-// User.find(function(users){
-//   if(users!=null){
-//     users.forEach(function(){
-//       console.log(user.name);
-//     });    
-//   }
-// });
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -51,19 +24,12 @@ app.use('/', routes);
 app.use('/static', express.static('public'));
 app.use('/users', users);
 
-
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
